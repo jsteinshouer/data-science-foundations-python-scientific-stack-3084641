31d44cf592fadf5f5403c1b28cdbc7d6561cf228
Jason Steinshouer <jason.steinshouer@gmail.com>
2023-02-05 04:33:07 +0000

    wip challenge 3

--- Ch03/challenge/mean_speed.py
+++ Ch03/challenge/mean_speed.py
@@ -1,3 +1,34 @@
 # What is the mean speed (mile/hour) in taxi.parquet?
 
 # - Run download_data.py to download the data
+
+# %%
+import pandas as pd
+import numpy as np
+
+parquet_file = 'taxi.parquet'
+df = pd.read_parquet(parquet_file)
+
+# df = pd.read_parquet(parquet_file, parse_dates=['tpep_pickup_datetime','tpep_dropoff_datetime'])
+len(df)
+# %%
+df.columns
+# %%
+df.info()
+# %%
+df.describe()
+
+# %%
+df[0:10]
+
+# %%
+# df["travel_time"] = (df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]) / pd.Timedelta(1, 'hour')
+travel_time_hours = (df["tpep_dropoff_datetime"] -
+                     df["tpep_pickup_datetime"]) / pd.Timedelta(1, 'hour')
+
+# %%
+speed = df["trip_distance"] / travel_time_hours
+
+# %%
+speed.mean()
+# %%
