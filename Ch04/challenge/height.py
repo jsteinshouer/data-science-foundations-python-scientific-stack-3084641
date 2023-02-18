ecc44d1d10ee1380b7ff52b79545dc83629f883e
Jason Steinshouer <jason.steinshouer@gmail.com>
2023-02-18 03:24:37 +0000

    ch4 challenge

--- Ch04/challenge/height.py
+++ Ch04/challenge/height.py
@@ -2,3 +2,39 @@
 #
 # - Sample data to a minute interval
 # - Markers should be blue if the height is below 100 meter, otherwise red
+
+# %%
+import folium
+import pandas as pd
+
+df = pd.read_csv(
+    'track.csv',
+    parse_dates=['time'],
+    index_col='time',
+)
+
+# %%
+
+
+def add_marker(row):
+    loc = tuple(row[['lat', 'lng']])
+    marker = folium.CircleMarker(
+        loc,
+        radius=5,
+        color='red' if row['height'] >= 100 else 'blue',
+        popup=row['height'],
+    )
+    marker.add_to(m)
+
+
+# %%
+center = [df['lat'].mean(), df['lng'].mean()]
+m = folium.Map(
+    location=center,
+    zoom_start=15,
+)
+min_df = df.resample('min').mean()
+min_df.apply(add_marker, axis=1)
+m
+
+# %%
