c0aacfeb127abf988da37cf44626bd3ca4f415e3
Jason Steinshouer <jason.steinshouer@gmail.com>
2023-02-24 02:57:14 +0000

    Ch 5 Challenge

--- Ch05/challenge/tip.py
+++ Ch05/challenge/tip.py
@@ -30,6 +30,38 @@ df = df[df.passenger_count != 0]
 df
 
 # %%
+# Add tip percent
+
+df['tip_percent'] = df['tip_amount'] / df['fare_amount'] * 100
+df['tip_percent']
+# %%
 # Create a bar chart of average tip % per passenger_count
 
+tip_df = (
+    df
+    .groupby(['passenger_count'], as_index=False)
+    ['tip_percent']
+    .median()
+)
+tip_df
 
+# tip_df['tip_index'] = 0;
+# tip_df.pivot(
+#     columns='passenger_count',
+#     index='tip_index',
+#     values='tip_percent'
+# ).plot.bar(rot=0)
+
+tip_df.plot.bar(x='passenger_count', y='tip_percent')
+# %%
+# Create a bar chart of average tip % per day of week
+df['day'] = df['tpep_pickup_datetime'].dt.day_name()
+df_by_day = (
+    df
+    .groupby(['day'], as_index=False)
+    ['tip_percent']
+    .median()
+)
+# df_by_day
+df_by_day.plot.bar(x='day', y='tip_percent')
+# %%
